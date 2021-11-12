77a4b2d6314882494eb67222fc2563158a756f5a
apple <apple@apples-MacBook-Pro.local>
2021-11-12 09:23:01 +0300

    styleing components and create custome hooks

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,22 +1,28 @@
 import React from 'react';
 
+import { createStage } from '../gameHelpers';
 import Stage from './Stage';
 import Display from './Display';
 import StartButton from './StartButton';
 
+import { StyledTetris, StyledTetrisWrapper } from './styles/StyledTetris';
+
 const Tetris = () => {
+	console.log(createStage());
 	return (
-		<div>
-			<Stage />
-			<aside>
-				<div>
-					<Display text='Score' />
-					<Display text='Rows' />
-					<Display text='Level' />
-				</div>
-			</aside>
-			<StartButton />
-		</div>
+		<StyledTetrisWrapper>
+			<StyledTetris>
+				<Stage stage={createStage()} />
+				<aside>
+					<div>
+						<Display text='Score' />
+						<Display text='Rows' />
+						<Display text='Level' />
+						<StartButton />
+					</div>
+				</aside>
+			</StyledTetris>
+		</StyledTetrisWrapper>
 	);
 };
 
